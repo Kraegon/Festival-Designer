c1ff3b87005af6559c36298630b72ba5715c74f7
Lesley van Hoek <lesleyvanhoek@hotmail.com>
2013-03-19 13:14:20 +0100

    THIS IS IT

--- src/simulator/DisplayableObjects/DisplayEntrance.java
+++ src/simulator/DisplayableObjects/DisplayEntrance.java
@@ -7,14 +7,14 @@ import java.awt.geom.Rectangle2D;
 
 public class DisplayEntrance extends DisplayObject
 {
+	Rectangle2D bounds;
+	
 	public DisplayEntrance(String name, Dimension size, boolean isSolid, Point2D location, String type)
 	{
 		super(name, size, false, location, "Entrance");
 	}
 
 	@Override
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public void drawObject(Graphics2D g) 
 	{
 		Rectangle2D obstacle = new Rectangle2D.Double(location.getX() - size.getWidth()/2, 
@@ -22,16 +22,6 @@ public class DisplayEntrance extends DisplayObject
 												      size.getWidth(), size.getHeight());
 		bounds = obstacle;
 		g.fill(obstacle);
-=======
-	public void drawObject(Graphics2D g) {
-		Rectangle2D entrance = new Rectangle2D.Double(location.getX(), location.getY(), size.getWidth(), size.getHeight());
-		g.fill(entrance);
->>>>>>> 01bded8f2c0b9519b8f9d91b74af0a95b1ef7d59
-=======
-	public void drawObject(Graphics2D g) {
-		Rectangle2D entrance = new Rectangle2D.Double(location.getX(), location.getY(), size.getWidth(), size.getHeight());
-		g.draw(entrance);
->>>>>>> parent of 92e4a7b... Committing on master because git forced me to
 	}
 
 	@Override
